e95990bf30ea4ff1556ddbe12d6162de9ed176ae
Morteza Nedaei <nedaei1375@gmail.com>
2021-01-05 15:31:51 +0330

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by MoNe on 10/16/1399 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
